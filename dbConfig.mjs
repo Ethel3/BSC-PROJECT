bc7898603306365a1cae1587f395ae7a7054eb2e
Ethel3 <akrasiethel4@gmail.com>
2023-08-26 02:38:36 +0000

    fix: Made requested changes

--- dbConfig.mjs
+++ dbConfig.mjs
@@ -1,11 +1,7 @@
 import { Pool } from 'pg';
 
 const Pool = new Pool ({
-    user: "username",
-    host: "localhost",
-    database: "database_name",
-    password: "password",
-    port: 5432,
+
 
 });
 
