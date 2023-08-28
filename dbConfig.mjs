9102668a86a6bf7606f01bb08fb4f1e33bdad51d
kwakubiney <kebiney@hotmail.com>
2023-08-28 22:33:53 +0000

    fix: remove controller logic and introduce environment variables
    Signed-off-by: kwakubiney <kebiney@hotmail.com>

--- dbConfig.mjs
+++ dbConfig.mjs
@@ -1,12 +1,11 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
-const Pool = new Pool ({
-    user: "username",
-    host: "localhost",
-    database: "database_name",
-    password: "password",
-    port: 5432,
-
+const pool = new Pool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 export default pool
