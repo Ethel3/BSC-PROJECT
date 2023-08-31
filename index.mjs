98a021c9dd21de4a221d65c6fad807acf2998c38
Ethel3 <akrasiethel4@gmail.com>
2023-08-31 15:02:49 +0000

    fix: made requested changes

--- index.mjs
+++ index.mjs
@@ -1,19 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { createRequire } from 'module';
-import connectToDb from './dbConfig.mjs';
+// import connectToDb from './dbConfig.mjs';
 import errorHandler from './middleware/errorHandler.js';
 import router from './routes/user.routes.js';
 
 const require = createRequire(import.meta.url)
-const app = express();
 require('dotenv').config()
-connectToDb()
+// connectToDb()
+const app = express();
+app.use(express.json());
 
 app.use("/api/user", router);
 
 app.use(errorHandler)
 
 const PORT = process.env.SERVER_PORT;
-app.use(bodyParser.json());
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
