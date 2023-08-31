0eb3b70c67242dd43d5372df4421508eafc278a7
Ethel3 <akrasiethel4@gmail.com>
2023-08-31 15:04:54 +0000

    fix: applied requested suggestions

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,10 @@
-const errorHandler = (err, req, res)=>{
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        status: 'fail',
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+const errorHandler = (err, req, res, next)=>{
+    const message = err.message || "Something went wrong"
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+        message,
+        success: false,
+        stack: process.env.NODE_ENV !== "development" ? null : err.stack
     });
 }
 
