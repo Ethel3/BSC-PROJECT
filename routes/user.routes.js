772e269f681ef27e14146c17b0e8c0d93cbd297c
Ethel3 <akrasiethel4@gmail.com>
2023-08-31 15:05:02 +0000

    fix: applied requested suggestions

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,11 @@
-// routes/userRoutes.mjs
 import express from 'express';
-import { createUser, updateUser, deleteUser, listUsers } from '../controllers/user.controller.js';
+import { createUser, updateUser, deleteUser, getUsers } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/', createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
-router.get('/', listUsers);
+router.get('/', getUsers);
 
 export default router;
