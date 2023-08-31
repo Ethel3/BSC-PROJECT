68de29fc112072a68e034f5c28cefd2cc46528d5
Ethel3 <akrasiethel4@gmail.com>
2023-08-31 05:19:44 +0000

    feat: created endpoints for user functionality

--- index.mjs
+++ index.mjs
@@ -2,11 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { createRequire } from 'module';
 import connectToDb from './dbConfig.mjs';
+import errorHandler from './middleware/errorHandler.js';
+import router from './routes/user.routes.js';
 
 const require = createRequire(import.meta.url)
 const app = express();
 require('dotenv').config()
 connectToDb()
+
+app.use("/api/user", router);
+
+app.use(errorHandler)
 
 const PORT = process.env.SERVER_PORT;
 app.use(bodyParser.json());
