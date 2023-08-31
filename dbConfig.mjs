3b7f4a54bd7bcfaff6137f0070e2af6315265218
Ethel3 <akrasiethel4@gmail.com>
2023-08-31 15:02:07 +0000

    fix: made some changes

--- dbConfig.mjs
+++ dbConfig.mjs
@@ -1,17 +1,14 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
 
-const connectToDb = async () => {
-        try{const pool = new Pool({
-            user: process.env.DB_USERNAME,
-            host: process.env.DB_HOST,
-            database: process.env.DB_DATABASE_NAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.DB_PORT
-    });
-    await pool.connect()}
-    catch(error){
-        console.log(error)
-    }
-}
-export default connectToDb
+dotenv.config()
+const pool = new Pool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
+
+export default pool
