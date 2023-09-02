e0a859f22abd0f7ad665edef549c9f417ec3d06e
kb <71296367+kwakubiney@users.noreply.github.com>
2023-09-02 21:12:21 +0000

    feat: build user functionality endpoints (#7)
    * setup: connect postgres to node 
    
    * feat: created endpoints for user functionality
    
    * fix: checked for user before updating and deleting
    
    * fix: remove comments and refac a few things
    
    ---------
    
    Signed-off-by: kwakubiney <kebiney@hotmail.com>
    Co-authored-by: Ethel3 <akrasiethel4@gmail.com>

--- dbConfig.mjs
+++ dbConfig.mjs
@@ -1,17 +1,12 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const connectToDb = async () => {
-        try{const pool = new Pool({
-            user: process.env.DB_USERNAME,
-            host: process.env.DB_HOST,
-            database: process.env.DB_DATABASE_NAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.DB_PORT
-    });
-    await pool.connect()}
-    catch(error){
-        console.log(error)
-    }
-}
-export default connectToDb
+const pool = new Pool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
+
+export default pool
