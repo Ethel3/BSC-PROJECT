e10d4e14ab6a5d9de279c3483165ecc3a3073af3
Ethel3 <akrasiethel4@gmail.com>
2023-09-13 21:33:44 +0000

    feat: implementing jwt

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import pool from '../dbConfig.mjs';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 export const createUser = async (req, res, next) => {
   try {
@@ -13,7 +14,7 @@ export const createUser = async (req, res, next) => {
     const result = await pool.query(query, values);
     token = jwt.sign(
       {
-        id: result.rows[0].id
+        id: result.rows[0].id, role: result.rows[0].role, username: result.rows[0].username, email: result.rows[0].email
       },
       process.env("SECRET"),
       { expiresIn: 360000 },
@@ -114,4 +115,51 @@ export const getUsers = async (req, res, next) => {
   }
 };
 
+//Login route function
+export const loginUser = async (req, res, next) =>{
+  try{
+    const { username, password } = req.body;
+    // Find the user by username in your database
+    const userQuery = `
+      SELECT * FROM users
+      WHERE username = $1;
+    `;
+    const userValues = [username];
+    const userResult = await pool.query(userQuery, userValues);
+
+    if (userResult.rows.length === 0) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const user = userResult.rows[0];
+    // Check if the provided password matches the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Create an Access Token
+    const accessToken = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '15m' }
+    );
+
+    // Create a Refresh Token (optional)
+    const refreshToken = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+    );
+
+    res.json({
+      message: 'Login successful',
+      accessToken,
+      refreshToken, // Optional
+    });
+  } catch (error) {
+  next(error);
+}
+};
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,26 @@
 import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  
+  try {
+    const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'Token missing from authorization header' });
   }
 
-  try {
     const decoded = jwt.verify(token, process.env("SECRET"));
-    req.id = decoded.id;
+    req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ message: 'User not authorized to access this resource' });
+  }
+};
+// check if the user is an admin
+export const isAdmin = (req, res, next)=> {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  } else {
+    return res.status(403).json({ message: 'Only admins can perform this operation' });
   }
 };
